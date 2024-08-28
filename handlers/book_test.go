07c0def519237e43d9b654fb695ffa52e3672514
awargnier <60512377+awargnier@users.noreply.github.com>
2024-08-28 20:39:19 +0200

    test

--- handlers/book_test.go
+++ handlers/book_test.go
@@ -1,24 +1,19 @@
 package handlers
 
 import (
+	"example/web-service-gin/models"
 	"testing"
 )
 
 func TestGetBook(t *testing.T) {
+	var books []models.Book
+
+	book := models.Book{Title: "vingt-mille lieux sous les mer", Author: "jules verne", Price: 2}
+
+	books = append(books, *GetBooksById(book.ID))
+
+	if book.ID != books.ID {
+		t.Error("Couldn't find correct book from bookstore")
+	}
 
 }
-
-// func TestHelloName(t *testing.T) {
-// 	name := "Gladys"
-// 	want := regexp.MustCompile(`\b` + name + `\b`)
-// 	t.Log(want)
-// }
-
-// TestHelloEmpty calls greetings.Hello with an empty string,
-// checking for an error.
-// func TestHelloEmpty(t *testing.T) {
-// 	msg, err := Hello("")
-// 	if msg != "" || err == nil {
-// 		t.Fatalf(`Hello("") = %q, %v, want "", error`, msg, err)
-// 	}
-// }
